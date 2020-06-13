325ca2deb4124d245a22454df820b67bf7862756
Takuya OHASHI <tky.ohashi@gmail.com>
2020-06-13 17:40:21 +0900

    Delete Matcher

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,7 @@ use clap::{crate_authors, crate_version, App, Arg};
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-
-pub trait MatcherTrait {
-    fn execute(&self, line: &str) -> bool;
-}
-
-pub struct FixedStringsMatcher {
-    pattern: String,
-}
-impl FixedStringsMatcher {
-    pub fn new(pattern: String) -> FixedStringsMatcher {
-        FixedStringsMatcher { pattern: pattern }
-    }
-}
-impl MatcherTrait for FixedStringsMatcher {
-    fn execute(&self, line: &str) -> bool {
-        line.contains(&self.pattern)
-    }
-}
+use grep_core::Matcher;
 
 fn main() {
     let matches = App::new("grep")
