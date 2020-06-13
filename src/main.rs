2c0012e61d67708228ba1608ed6b3cefcb5efc95
Takuya OHASHI <tky.ohashi@gmail.com>
2020-06-13 10:09:18 +0900

    Parse argments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{crate_authors, crate_version, App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("grep")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Search for PATTERNS in each FILE")
+        .arg(
+            Arg::with_name("fixed-strings")
+                .short("F")
+                .long("fixed-strings")
+                .help("PATTERNS are strings"),
+        )
+        .arg(
+            Arg::with_name("PATTERNS")
+                .help("use PATTERNS for matching")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("FILES")
+                .help("take PATTERNS from FILES")
+                .required(true)
+                .multiple(true)
+                .index(2),
+        )
+        .get_matches();
+
+    println!("{:?}", matches);
 }
